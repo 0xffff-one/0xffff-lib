cc1880af99a5b82a98f22ef3201028a38530cc70
Iosmanthus Teng <myosmanthustree@gmail.com>
2019-08-05 23:53:57 +0800

    fix .clang-format
    Signed-off-by: Iosmanthus Teng <myosmanthustree@gmail.com>

--- src/collections/vector.h
+++ src/collections/vector.h
@@ -6,60 +6,54 @@
 
 template <typename T>
 class Vec {
-  public:
-    Vec();
-    virtual ~Vec();
+ public:
+  Vec();
+  virtual ~Vec();
 
-  private:
-    T* buf;
-    size_t len;
+ private:
+  T* buf;
+  size_t len;
 
-  public:
-    // Get the length of the vector
-    size_t size();
+ public:
+  // Get the length of the vector
+  size_t size();
 
-    // Examine if the vector is empty.
-    bool is_empty();
+  // Examine if the vector is empty.
+  bool is_empty();
 
-    // Push data back to the vector
-    void push(T data);
+  // Push data back to the vector
+  void push(T data);
 
-    // Pop out an element from the back of vector
-    std::optional<T> pop();
+  // Pop out an element from the back of vector
+  std::optional<T> pop();
 
-    // Overloading index operator
-    T operator[](size_t index);
+  // Overloading index operator
+  T operator[](size_t index);
 };
 
-  template <typename T>
-inline Vec<T>::Vec()
-{
+template <typename T>
+inline Vec<T>::Vec() {
   this->buf = nullptr;
   len = 0;
 }
 
-  template <typename T>
-inline size_t Vec<T>::size()
-{
+template <typename T>
+inline size_t Vec<T>::size() {
   return this->len;
 }
 
-  template <typename T>
-inline Vec<T>::~Vec()
-{
-  if (this->len == 0)
-    delete[] this->buf;
+template <typename T>
+inline Vec<T>::~Vec() {
+  if (this->len == 0) delete[] this->buf;
 }
 
-  template <typename T>
-inline bool Vec<T>::is_empty()
-{
+template <typename T>
+inline bool Vec<T>::is_empty() {
   return this->len == 0;
 }
 
-  template <typename T>
-void Vec<T>::push(T data)
-{
+template <typename T>
+void Vec<T>::push(T data) {
   T* new_buf = new T[this->len + 1];
   std::copy(this->buf, this->buf + this->len, new_buf);
   new_buf[this->len] = data;
@@ -70,18 +64,15 @@ void Vec<T>::push(T data)
   this->len++;
 }
 
-  template <typename T>
-std::optional<T> Vec<T>::pop()
-{
-  if (this->len > 0)
-    return this->buf[this->len-- - 1];
+template <typename T>
+std::optional<T> Vec<T>::pop() {
+  if (this->len > 0) return this->buf[this->len-- - 1];
 
   return {};
 }
 
-  template <typename T>
-T Vec<T>::operator[](size_t idx)
-{
+template <typename T>
+T Vec<T>::operator[](size_t idx) {
   if (idx < this->len)
     return this->buf[idx];
   else
