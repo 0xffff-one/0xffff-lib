8b3c0754364ddc0e6a6cad60ca6dcd146b179e0c
Iosmanthus Teng <myosmanthustree@gmail.com>
2019-08-11 20:51:16 +0800

    Add `LinkedList` and its tests
    Signed-off-by: Iosmanthus Teng <myosmanthustree@gmail.com>

---
+++ tests/test-linked-list.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+#include "src/collections/linked-list.h"
+
+TEST(TestLinkedList, TestPush) {
+  LinkedList<int> list;
+  auto cases = std::vector<int>({1, 2, 3, 4, 5});
+
+  for (auto i : cases) {
+    list.push(i);
+  }
+
+  for (size_t i = 0; i < cases.size(); ++i) {
+    EXPECT_EQ(cases[i], list[i]);
+  }
+}
