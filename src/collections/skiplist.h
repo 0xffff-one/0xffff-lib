d4c9973a2512c7442a2584a361dc023e977f3f23
fengkx <liangkx8237@gmail.com>
2019-11-13 13:55:49 +0800

    fix: bug when insert repacle

--- src/collections/skiplist.h
+++ src/collections/skiplist.h
@@ -6,6 +6,8 @@
 #include <cstring>
 #include <functional>
 
+#define LEVEL_RATE 0.5
+
 template <typename K, typename V, typename Comparator = std::less<K> >
 class SkipList {
  private:
@@ -31,7 +33,7 @@ class SkipList {
       std::copy(other.level, other.level + level_num, this->level);
     }
 
-    virtual ~Node() { delete this->level; }
+    virtual ~Node() { delete[] this->level; }
   };
 
   static const size_t MAXLEVEL = 32;
@@ -48,7 +50,7 @@ class SkipList {
   SkipList &operator=(const SkipList<K, V, Comparator> &other);
   virtual ~SkipList();
 
-  Node *insert(K key, V value, bool replace= false);
+  Node *insert(K key, V value, bool replace = false);
   Node *search(const K &key);
   bool remove(const K &key);
   size_t size() const;
@@ -83,7 +85,7 @@ template <typename K, typename V, typename Comparator>
 int SkipList<K, V, Comparator>::randomLevel() {
   int level = 1;
 
-  while ((random() & 0xFFFF) < (0.25 * 0xFFFF)) level += 1;
+  while ((random() & 0xFFFF) < (LEVEL_RATE * 0xFFFF)) level += 1;
 
   return (level < signed(MAXLEVEL)) ? level : MAXLEVEL;
 }
@@ -103,11 +105,13 @@ SkipList<K, V, Comparator>::SkipList(const SkipList<K, V, Comparator> &other) {
 
 template <typename K, typename V, typename Comparator>
 typename SkipList<K, V, Comparator>::Node *SkipList<K, V, Comparator>::insert(
-        K key, V value, bool replace) {
+    K key, V value, bool replace) {
   // store the level and the position to insert After
   Node *update[MAXLEVEL], *x;
   Comparator lessThan = this->cmp;
+  bool replacedTag = false;
 
+  Node *r = nullptr;
   x = this->header;
   for (int i = this->largestLevel - 1; i >= 0; i--) {
     while (x->level[i].forward && /*this level enable*/
@@ -116,15 +120,21 @@ typename SkipList<K, V, Comparator>::Node *SkipList<K, V, Comparator>::insert(
     }
 
     // equal key conflict
-    if (x->level[i].forward &&
-        !lessThan(x->level[i].forward->key, key) &&
+    if (x->level[i].forward && !lessThan(x->level[i].forward->key, key) &&
         !lessThan(key, x->level[i].forward->key)) {
-        if(!replace) return nullptr;
-        else
-            this->len--; // minus one first
+      if (!replace)
+        return nullptr;
+      else {
+        x->level[i].forward->value = value;
+        replacedTag = true;
+        if (r == nullptr) r = x->level[i].forward;
+      }
     }
     update[i] = x;
   }
+
+  // replaced no need to update and create new node
+  if (replacedTag) return r;
 
   int level_num = randomLevel();
   if (level_num > this->largestLevel) {
@@ -135,7 +145,7 @@ typename SkipList<K, V, Comparator>::Node *SkipList<K, V, Comparator>::insert(
   }
   x = new Node(key, value, level_num);
 
-  for (int i = 0; i < this->largestLevel; i++) {
+  for (int i = 0; i < level_num; i++) {
     x->level[i].forward = update[i]->level[i].forward;
     update[i]->level[i].forward = x;
   }
@@ -149,53 +159,49 @@ inline size_t SkipList<K, V, Comparator>::size() const {
   return this->len;
 }
 
-template<typename K, typename V, typename Comparator>
-typename SkipList<K, V, Comparator>::Node *SkipList<K, V, Comparator>::search(const K &key) {
-    Node *x = this->header, *ret = nullptr;
-    Comparator less = this->cmp;
-    for(int i=this->largestLevel-1;i>=0;i--) {
-        while (x->level[i].forward &&
-            less(x->level[i].forward->key, key)) {
-            x = x->level[i].forward;
-        }
-
-        // equal just return
-        if(x->level[i].forward &&
-            !less(x->level[i].forward->key, key) &&
-            !less(key, x->level[i].forward->key)) {
-            ret = x->level[i].forward;
-            return ret;
-        }
+template <typename K, typename V, typename Comparator>
+typename SkipList<K, V, Comparator>::Node *SkipList<K, V, Comparator>::search(
+    const K &key) {
+  Node *x = this->header, *ret = nullptr;
+  Comparator less = this->cmp;
+  for (int i = this->largestLevel - 1; i >= 0; i--) {
+    while (x->level[i].forward && less(x->level[i].forward->key, key)) {
+      x = x->level[i].forward;
     }
-    return ret;
+
+    // equal just return
+    if (x->level[i].forward && !less(x->level[i].forward->key, key) &&
+        !less(key, x->level[i].forward->key)) {
+      ret = x->level[i].forward;
+      return ret;
+    }
+  }
+  return ret;
 }
 
-template<typename K, typename V, typename Comparator>
-bool SkipList<K, V, Comparator>::remove(const K& key) {
-    Node *x = this->header;
-    bool ret = false;
-    Comparator less = this->cmp;
-    for(int i=this->largestLevel-1;i>=0;i--) {
-        while (x->level[i].forward &&
-               less(x->level[i].forward->key, key)) {
-            x = x->level[i].forward;
-        }
-
-        // equal just return
-        if(x->level[i].forward &&
-           !less(x->level[i].forward->key, key) &&
-           !less(key, x->level[i].forward->key)) {
-//            ret = x->level[i].forward;
-            Node *del = x->level[i].forward;
-            x->level[i].forward = del->level[i].forward;
-            delete del;
-            ret = true;
-            this->len--;
-            return ret;
-        }
+template <typename K, typename V, typename Comparator>
+bool SkipList<K, V, Comparator>::remove(const K &key) {
+  Node *x = this->header;
+  bool ret = false;
+  Comparator less = this->cmp;
+  for (int i = this->largestLevel - 1; i >= 0; i--) {
+    while (x->level[i].forward && less(x->level[i].forward->key, key)) {
+      x = x->level[i].forward;
     }
-    return ret;
-}
 
+    // equal just return
+    if (x->level[i].forward && !less(x->level[i].forward->key, key) &&
+        !less(key, x->level[i].forward->key)) {
+      //            ret = x->level[i].forward;
+      Node *del = x->level[i].forward;
+      x->level[i].forward = del->level[i].forward;
+      delete del;
+      ret = true;
+      this->len--;
+      return ret;
+    }
+  }
+  return ret;
+}
 
 #endif  // SKIP_LIST
--- tests/test-skiplist.cc
+++ tests/test-skiplist.cc
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 
-#include "src/collections/skiplist.h"
 #include <string>
+
+#include "src/collections/skiplist.h"
 
 TEST(SkipList, Constrcutor) {
   auto lst = SkipList<std::string, std::string>(std::less<std::string>());
@@ -9,29 +10,41 @@ TEST(SkipList, Constrcutor) {
 }
 
 TEST(SkipList, Insert) {
-    auto lst = SkipList<std::string, std::string>(std::less<std::string>());
-    lst.insert("key", "value");
-    EXPECT_EQ(lst.size(), 1);
-    auto np = lst.search("key");
-    EXPECT_EQ(np->value, "value");
-    lst.insert("abc", "ABC");
-    EXPECT_EQ(lst.size(), 2);
-    lst.insert("key", "new value", 1);
-    EXPECT_EQ(lst.size(), 2);
-    np = lst.search("key");
-    EXPECT_EQ(np->value, "new value");
+  auto lst = SkipList<std::string, std::string>(std::less<std::string>());
+  lst.insert("key", "value");
+  EXPECT_EQ(lst.size(), 1);
+  auto np = lst.search("key");
+  EXPECT_EQ(np->value, "value");
+  lst.insert("abc", "ABC");
+  EXPECT_EQ(lst.size(), 2);
+  lst.insert("key", "new value", 1);
+  EXPECT_EQ(lst.size(), 2);
+  np = lst.search("key");
+  EXPECT_EQ(np->value, "new value");
+}
+
+TEST(SkipList, InsertLargeAmount) {
+  auto lst = SkipList<std::string, std::string>(std::less<std::string>());
+  const int N = 2 << 10;
+  for (int i = 1; i <= N; i++) {
+    std::string key = "key" + std::to_string(i);
+    std::string value = "value" + std::to_string(i);
+    auto np = lst.insert(key, value);
+    EXPECT_EQ(np->value, value);
+  }
+  EXPECT_EQ(lst.size(), N);
 }
 
 TEST(SkipList, Remove) {
-    auto lst = SkipList<std::string, std::string>(std::less<std::string>());
-    lst.insert("key", "value");
-    EXPECT_EQ(lst.size(), 1);
-    auto np = lst.search("key");
-    EXPECT_EQ(np->value, "value");
+  auto lst = SkipList<std::string, std::string>(std::less<std::string>());
+  lst.insert("key", "value");
+  EXPECT_EQ(lst.size(), 1);
+  auto np = lst.search("key");
+  EXPECT_EQ(np->value, "value");
 
-    bool ret = lst.remove("key");
-    EXPECT_TRUE(ret);
-    EXPECT_EQ(lst.size(), 0);
-    np = lst.search("key");
-    EXPECT_EQ(np, nullptr);
+  bool ret = lst.remove("key");
+  EXPECT_TRUE(ret);
+  EXPECT_EQ(lst.size(), 0);
+  np = lst.search("key");
+  EXPECT_EQ(np, nullptr);
 }
